5a5a11040428f7637bfcf17cdb12e61ce3e6718f
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-09-09 16:31:48 -0300

    Front Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Iniciar from "./components/Iniciar.js";
+import Questionario from "./components/Questionario.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [page, setPage] = useState("iniciar");
+    return (
+      <>{page === "iniciar" ? <Iniciar setPage={setPage} /> : <Questionario />}</>
+    );
 }
-
-export default App;
